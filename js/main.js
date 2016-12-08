651f4bc425f684bfbb75beb1440fee9af60e502e
leeseoki <leeseoki23@gmail.com>
2016-12-08 17:45:39 +0700

    updated content for contact section

--- js/main.js
+++ js/main.js
@@ -82,4 +82,30 @@ jQuery(function($) {
       });
     });
   });
+
+  //Added Google Map
+  var latitude = $('#google-map').data('latitude')
+  var longitude = $('#google-map').data('longitude')
+  function initMap() {
+    var myLatlng = new google.maps.LatLng(latitude,longitude);
+    var mapOptions = {
+      zoom: 13,
+      scrollwheel: false,
+      center: myLatlng
+    };
+    var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+    var contentString = '';
+    var infowindow = new google.maps.InfoWindow({
+      content: '<div class="map-content"><div class="address">' + $('.address').html() + '</div></div>'
+    });
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      icon: '../images/icons/icon-map.png',
+      map: map
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map,marker);
+    });
+  }
+  google.maps.event.addDomListener(window, 'load', initMap);
 });
